e462dbbf46a2d71aeb3999b0909218959cbf2e71
Deepankar Sharma <deepankar.sharma@gmail.com>
2010-07-25 18:19:43 -0400

    Added some comments explaining the solution

--- Problem_0003/problem_0003_dawgmatix.py
+++ Problem_0003/problem_0003_dawgmatix.py
@@ -1,5 +1,43 @@
 import unittest
 
+#Analysis
+#0-0
+
+# 1-1
+# 1+0
+# X
+# X
+# a
+
+# 2-2
+# 1+1
+# XX xx
+# XX xx
+# aa b
+
+# 3-3
+# 1+2
+# XXX Xxx xxX
+# XXX Xxx xxX
+# aaa ab  ba
+
+# 4-5
+# 1+4
+# XXXX xxXX XxxX XXxx xxxx
+# XXXX xxXX XxxX XXxx xxxx
+# aaaa baa  aba  aab  bb
+
+
+# 5-7
+# 1 + 4 + 2
+# XXXXX xXXX XxXXX XXxX XXXx xxX
+# XXXXX xXXX XxXXX XXxX XXXx xxX
+
+# General form :
+# if n = 0
+#  return 0
+# else
+#  return 1 + (n - 2 + 1) + (n - 2x2 + 1) + (n - 2x2x2 + 1) ....
 
 def count_brick_configs (x):
     if (x == 0):
