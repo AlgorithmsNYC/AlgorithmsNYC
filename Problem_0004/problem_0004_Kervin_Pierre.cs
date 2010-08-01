e3d4c127f9a353e7a8d807fe126bb30575e40277
kervinpierre <kervin@adevsoft.com>
2010-07-31 20:19:42 -0400

    Adding a C# solution.  This was tested with the examples in the problem description.  Tested with C# 4.0, but 3.0 or higher should work.  The relevant code is a single line.

---
+++ Problem_0004/problem_0004_Kervin_Pierre.cs
@@ -0,0 +1,32 @@
+﻿// Kervin Pierre - kervin@adevsoft.com - 2010-07-31
+using System;
+using System.Linq;
+
+namespace prob4
+{
+    class Program
+    {
+        // Solution - I've only tested with the examples in the problem description
+        public static double gravitationalAcceleration(int[] height, int T)
+        {
+            return 1.0 / Math.Pow(T / height.Select(i => (double)i).Sum(i => Math.Sqrt((double)2 * i)), 2.0);
+        }
+
+        // Driver
+        static void Main(string[] args)
+        {
+            int[] test = new int[] { 16, 23, 85, 3, 35, 72, 96, 88, 2, 14, 63 };
+            Console.WriteLine("{0}", gravitationalAcceleration(test, 30));
+
+            test = new int[]{ 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5 };
+            Console.WriteLine("{0}", gravitationalAcceleration(test, 12));
+
+            test = new int[] { 8, 8 };
+            Console.WriteLine("{0}", gravitationalAcceleration(test, 3));
+
+            test = new int[] {3,1,3,1,3};
+            Console.WriteLine("{0}", gravitationalAcceleration(test, 12));
+        }
+
+    }
+}
