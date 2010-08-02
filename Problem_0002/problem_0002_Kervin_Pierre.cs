777936177ef5808f7087e0c48b6b890a7f51c5db
kervinpierre <kervin@adevsoft.com>
2010-08-02 18:53:21 -0400

    Adding C# solution for problem 2.

---
+++ Problem_0002/problem_0002_Kervin_Pierre.cs
@@ -0,0 +1,31 @@
+﻿// Kervin Pierre - kervin@adevsoft.com - 2010-08-02
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prob2
+{
+    class Program
+    {
+        // Solution
+        public static string encrypt(string message)
+        {
+            string res = "";
+            int lastChar = (int)'a';
+
+            Dictionary<char, char> dict = new Dictionary<char, char>();
+            message.ToList().ForEach(c => res += dict.ContainsKey(c) ? dict[c] : dict[c] = (char)(lastChar++));
+
+            return res;
+        }
+
+        // Driver
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", encrypt("hello"));
+            Console.WriteLine("{0}", encrypt("abcd"));
+            Console.WriteLine("{0}", encrypt("topcoder"));
+            Console.WriteLine("{0}", encrypt("encryption"));
+        }
+    }
+}
